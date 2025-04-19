d40268b8d3b8339389af1965935781e54d935d5e
jhonatan rodriguez <48069972+jhonatanrodriguez@users.noreply.github.com>
2025-04-19 01:04:04 -0500

    Add files via upload

---
+++ FrontEnd/src/data/CountryData.js
@@ -0,0 +1,81 @@
+import React from "react";
+const data2 = [
+  {
+    name: "United States",
+    country: "us",
+    value: 2346
+  },
+  {
+    name: "Japan",
+    country: "jp",
+    value: 571
+  },
+  {
+    name: "India",
+    country: "in",
+    value: 464
+  },
+  {
+    name: "China",
+    country: "cn",
+    value: 397
+  },
+  {
+    name: "Germany",
+    country: "de",
+    value: 317
+  },
+  {
+    name: "Russian Federation",
+    country: "ru",
+    value: 309
+  },
+  {
+    name: "France",
+    country: "fr",
+    value: 297
+  },
+  {
+    name: "Korea, Republic of",
+    country: "kr",
+    value: 244
+  },
+  {
+    name: "United Kingdom",
+    country: "gb",
+    value: 196
+  },
+  {
+    name: "Iran",
+    country: "ir",
+    value: 193
+  },
+  {
+    name: "Indonesia",
+    country: "id",
+    value: 192
+  },
+  {
+    name: "Brazil",
+    country: "br",
+    value: 190
+  },
+  {
+    name: "Turkey",
+    country: "tr",
+    value: 184
+  },
+  {
+    name: "Mexico",
+    country: "mx",
+    value: 166
+  },
+  {
+    name: "Canada",
+    country: "ca",
+    value: 157
+  }
+];
+
+export { data2 };
+
